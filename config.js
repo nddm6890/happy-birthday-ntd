959ca8570425773a280ab3c9612a48fa38a51d01
nddm6890 <nguyendinhducmanh150309@gmail.com>
2026-09-01 21:53:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const CONFIG_PRIVATE = {
     // Thay đổi mật khẩu tại đây
     PASSWORD: "1609", 
     // Thay đổi link quà tại đây
-    GIFT_LINK: "https://youtu.be/eco6rsWwSH8?si=xXD7dXt4ELfEuFfe"
+    GIFT_LINK: "watch.html"
 };
