831d4f5acfa53384dac77da6cfbe114c67160700
nddm6890 <nguyendinhducmanh150309@gmail.com>
2026-09-01 21:42:13 +0700

    Add private configuration with password and gift link

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG_PRIVATE = {
+    // Thay đổi mật khẩu tại đây
+    PASSWORD: "1609", 
+    // Thay đổi link quà tại đây
+    GIFT_LINK: "https://gist.githubusercontent.com/nddm6890/ef371d94108f9ebaf54fb7fe8acdf58f/raw/270ea241ebb7667d18d0dbed254031b58f96db4c/gistfile1.txt"
+};
