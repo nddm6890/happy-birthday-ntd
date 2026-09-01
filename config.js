eced48f02e728924badde99ecbcfb30c7d84519c
nddm6890 <nguyendinhducmanh150309@gmail.com>
2026-09-01 21:47:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const CONFIG_PRIVATE = {
     // Thay đổi mật khẩu tại đây
     PASSWORD: "1609", 
     // Thay đổi link quà tại đây
-    GIFT_LINK: "(ds)https://gist.githubusercontent.com/nddm6890/ef371d94108f9ebaf54fb7fe8acdf58f/raw/270ea241ebb7667d18d0dbed254031b58f96db4c/gistfile1.txt"
+    GIFT_LINK: "https://gist.githubusercontent.com/ef371d94108f9ebaf54fb7fe8acdf58f/raw/270ea241ebb7667d18d0dbed254031b58f96db4c/gistfile1.txt"
 };
